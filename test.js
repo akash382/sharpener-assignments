80030aac20e1898b3bdf5f3686ae656165d0e1c5
akash382 <84083983+akash382@users.noreply.github.com>
2023-04-22 13:55:43 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,6 +1,23 @@
-
+/*get elements by classname
 var items = document.getElementsByClassName("list-group-item");
 items[2].style.backgroundColor="green";
+
+
+
 for(let i=0;i< items.length;i++){
     items[i].style.fontWeight="900";
+    
+}
+
+*/
+
+//getElementsByTagName 
+var items = document.getElementsByTagName('li');
+items[2].style.backgroundColor="green";
+
+
+
+for(let i=0;i< items.length;i++){
+    items[i].style.fontWeight="900";
+    
 }
