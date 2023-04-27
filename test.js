cab690af2f4d0a14ea8263316ed5ff997ed095f7
akash382 <84083983+akash382@users.noreply.github.com>
2023-04-27 17:57:25 +0530

    new changes

--- test.js
+++ test.js
@@ -12,6 +12,7 @@ for(let i=0;i< items.length;i++){
 */
 
 //getElementsByTagName 
+/*\
 var items = document.getElementsByTagName('li');
 items[2].style.backgroundColor="green";
 
@@ -19,5 +20,20 @@ items[2].style.backgroundColor="green";
 
 for(let i=0;i< items.length;i++){
     items[i].style.fontWeight="900";
+    
+}
+*/
+
+//queryselector 
+var secItem = document.querySelector(".list-group-item:nth-child(2)");
+secItem.style.backgroundColor="green"
+var thirdItem = document.querySelector(".list-group-item:nth-child(3)")
+thirdItem.style.display="none"
+
+var items= document.querySelectorAll(".list-group-item")
+items[1].style.color="green"
+var odd=document.querySelectorAll("li:nth-child(odd)");
+for(var i =0;i<odd.length;i++){
+    odd[i].style.backgroundColor="green"
     
 }
