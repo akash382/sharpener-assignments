938db6aa8bceb2fa2d02800903f82bd1e7e7fd94
akash382 <84083983+akash382@users.noreply.github.com>
2023-04-30 16:20:57 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,39 +1,60 @@
-/*get elements by classname
-var items = document.getElementsByClassName("list-group-item");
-items[2].style.backgroundColor="green";
+//traversing the Dom
+var itemList = document.querySelector("#items");
+//parent node and ParentELement is the same thing 
+
+//itemList.parentElement.style.backgroundColor = "#f4f4f4";
 
 
+//child node
+//itemList.lastElementChild.style.color ="red";
 
-for(let i=0;i< items.length;i++){
-    items[i].style.fontWeight="900";
-    
-}
+//firstchild
+itemList.firstChild.style.color="red";
 
-*/
+//firstELementChild
+itemList.firstElementChild.textContent="helo1";
 
-//getElementsByTagName 
-/*\
-var items = document.getElementsByTagName('li');
-items[2].style.backgroundColor="green";
+//lastChild
+itemList.lastChild.textContent="helo4";
+
+//nextSibling
+itemList.nextSibling.style.color="green";
+
+//nextELementSIbling but in this there is no nextSIbling
+itemList.nextElementSibling.style.backgroundColor="yellow";
+
+itemList.previousSibling.style.backgroundColor="green";
+
+//createELement
+//create a div
+var newDiv = document.createElement('div');
+//adding a classname
+newDiv.className="HELLO";
+//adding an id
+newDiv.id="HELLO1";
+newDiv.setAttribute("title","Hello Div");
+console.log(newDiv);
+
+var newDivText = document.createTextNode("hello");
+
+//add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector("header .container");
+var h1=document.querySelector("header h1")
+
+container.insertBefore(newDiv,h1);
 
 
+var newDiv2 = document.createElement("div");
+newDiv2.className="second";
+newDiv2.id="secondId";
+newDiv2.setAttribute("title","Hello second");
 
-for(let i=0;i< items.length;i++){
-    items[i].style.fontWeight="900";
-    
-}
-*/
+var newDiv2Text = document.createTextNode("hello");
+newDiv2.appendChild(newDiv2Text);
 
-//queryselector 
-var secItem = document.querySelector(".list-group-item:nth-child(2)");
-secItem.style.backgroundColor="green"
-var thirdItem = document.querySelector(".list-group-item:nth-child(3)")
-thirdItem.style.display="none"
+var container = document.querySelector("header .items");
+var h2 = document.querySelector("header h2");
+container.insertBefore(newDiv2,h2);
 
-var items= document.querySelectorAll(".list-group-item")
-items[1].style.color="green"
-var odd=document.querySelectorAll("li:nth-child(odd)");
-for(var i =0;i<odd.length;i++){
-    odd[i].style.backgroundColor="green"
-    
-}
